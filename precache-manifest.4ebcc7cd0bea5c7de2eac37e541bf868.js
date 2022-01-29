97c20c3d4051dd11281b2ff24ce812dc40886d3e
soyabbeig <soyabbeig7932@gmail.com>
2022-01-29 12:36:19 +0530

    Updates

---
+++ precache-manifest.4ebcc7cd0bea5c7de2eac37e541bf868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf8e0b4b7ab1c0d106fff9b3dced920",
+    "url": "/todolist-react/index.html"
+  },
+  {
+    "revision": "c47d420dfe37aa4dba8a",
+    "url": "/todolist-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "a6b5d126f298651bed92",
+    "url": "/todolist-react/static/js/2.c1fe5449.chunk.js"
+  },
+  {
+    "revision": "68c47fc33538f58652f4",
+    "url": "/todolist-react/static/js/3.987c3a98.chunk.js"
+  },
+  {
+    "revision": "c47d420dfe37aa4dba8a",
+    "url": "/todolist-react/static/js/main.59c1d669.chunk.js"
+  },
+  {
+    "revision": "7070f3a425595d25c0a1",
+    "url": "/todolist-react/static/js/runtime~main.85f718db.js"
+  }
+]);
